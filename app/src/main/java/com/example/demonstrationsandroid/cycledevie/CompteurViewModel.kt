43576f7075e81c39e517b5e4adf63e7576a5909e
agahnim13@gmail.com <etienne.cassin@sweetdreams.studio>
2022-12-16 16:42:20 +0100

    ajout de la demo livedata

--- app/src/main/java/com/example/demonstrationsandroid/cycledevie/CompteurViewModel.kt
+++ app/src/main/java/com/example/demonstrationsandroid/cycledevie/CompteurViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.demonstrationsandroid.cycledevie
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CompteurViewModel : ViewModel() {
-    var cpt = 0
+    var cpt = MutableLiveData(0)
 
     fun increment() {
-        cpt++
+        cpt.value = cpt.value?.plus(1)
+        // Equivalent a cpt++ mais avec livedata
+    }
+
+    fun decrementer() {
+        cpt.value = cpt.value?.minus(1)
     }
 }
--- app/src/main/java/com/example/demonstrationsandroid/cycledevie/CycleDeVieFragmentFragment.kt
+++ app/src/main/java/com/example/demonstrationsandroid/cycledevie/CycleDeVieFragmentFragment.kt
@@ -25,14 +25,36 @@ class CycleDeVieFragmentFragment : Fragment() {
             container,
             false)
 
-        viewmodel = ViewModelProvider(this).get(CompteurViewModel::class.java)
+        viewmodel = ViewModelProvider(this)[CompteurViewModel::class.java]
         binding.vm = viewmodel
+
+        /*
+         * Quel interet ?
+         * 1. Ne plus avoir besoin d'ecrire binding.vm = viewmodel dans chaque listener
+         * 2. La valeur de cpt peut etre observee par plusieurs ecrans (activity ou fragment)
+         */
+        viewmodel.cpt.observe(viewLifecycleOwner) { binding.vm = viewmodel }
+
+        viewmodel.cpt.observe(viewLifecycleOwner) {
+            if (it == 10) {
+                binding.monLayoutColore.background = resources.getDrawable(R.drawable.chaton)
+            }
+        }
 
         // binding.tvIncrement.text = savedInstanceState?.getString("compteur")?:"0"
 
         binding.btnPlusUn.setOnClickListener {
             viewmodel.increment()
-            binding.vm = viewmodel
+            // binding.vm = viewmodel // Plus necessaire grace a livedata
+        }
+
+        binding.btnIncrementerParDeux.setOnClickListener{
+            viewmodel.increment()
+            viewmodel.increment()
+        }
+
+        binding.btnDecrementer.setOnClickListener{
+            viewmodel.decrementer()
         }
 
         return binding.root
